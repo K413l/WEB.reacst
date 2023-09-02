add4927af7fa57d0e47cfaabdcbd60bb4350d5b7
K413l <kalel.schlichting@gmail.com>
2023-09-01 22:39:33 -0300

    asdasdasdasdasd

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,8 @@
+
+export const ListaProdutos = [
+    {id:1, nome:"PenDrive", desc:"16GB USB 3.0", img:"https://picsum.photos/100/100", preco:80},
+    {id:2, nome:"Mouse", desc:"Logitech", img:"https://picsum.photos/40/50", preco:100},
+    {id:3, nome:"Monitor", desc:"Asus Full HD", img:"https://picsum.photos/30/100", preco:800},
+    {id:4, nome:"Headset", desc:"Razer", img:"https://picsum.photos/30/100", preco:500},
+    
+]
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,6 +1,41 @@
+import { ListaProdutos } from "../components/ListaProdutos";
 
 export default function Produtos() {
+  
+  document.title = 'produtos';
+
+  
   return (
-    <div>Produtos</div>
+  <div>
+      <h1>Produtos</h1>
+
+      <div>
+          
+          <table>
+            
+            <tr>
+                <th>ID</th>
+                <th>NOME</th>
+                <th>DESCRIÇÃO</th>
+                <th>IMAGEM</th>
+                <th>PREÇO</th>
+                <th>EDITAR/EXCLUIR</th>
+            </tr>
+
+            {ListaProdutos.map( (item,indice)=>(
+              <tr key={indice}>
+                <td>{item.id}</td>
+                <td>{item.nome}</td>
+                <td>{item.desc}</td>
+                <td><img src={`${item.img}`} alt={`${item.desc}`} /></td>
+                <td>{item.preco}</td>
+                <td>Editar / Excluir</td>
+              </tr>
+            ))}
+
+          </table>
+
+      </div>
+  </div>
   )
 }
